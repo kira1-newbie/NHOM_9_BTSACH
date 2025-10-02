6cdd08fdae869dc638ba99589802c27c67629bb8
kelvinhuy313-code <kelvinhuy313@gmail.com>
2025-10-03 00:21:47 +0700

    Add files via upload

---
+++ TEST.java
@@ -0,0 +1,17 @@
+public class TEST {
+
+ public static void main(String [] args)
+
+ {
+
+  BOOK sach=new BOOK("BOOK01","FANTASTIC","CONAN",2020,10);
+
+  sach.displayInfo();
+
+  sach.setAuthor("Trung");
+
+  System.out.println("AUTHOR 1: "+sach.getAuthor());
+
+ }
+
+}
